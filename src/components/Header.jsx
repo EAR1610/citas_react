f56235ed40cd029926d601b40c9e9f1b916d9f70
EAR1610 <alexreynosoruano741@gmail.com>
2022-12-20 21:06:35 -0600

    Agregando el nuevo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@
 const Header = () => {
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes { ' ' }
+            Seguimiento Pacientes { ' ' } Premium { ' ' }
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
     )
